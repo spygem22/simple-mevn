0cdc42e08683dd5a8aacb91f9e56062d93843f76
spygem22 <jeffreyadan75@gmail.com>
2021-05-04 02:31:28 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import AddPost from "../views/Add.vue";
+import Post from "../views/Post.vue";
+import Edit from "../views/EditPost.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
+  },
+  {
+    path: '/add-post',
+    name: 'add-post',
+    component: AddPost
+  },
+  {
+    path: '/post/:id',
+    name: 'post',
+    component: Post
+  },
+  {
+    path: '/edit-post/:id',
+    name: 'edit-post',
+    component: Edit
   },
   {
     path: '/about',
